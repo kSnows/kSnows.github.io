a4221e6bc76730751e820849d8533ada6faa2cce
Kira Snows <KiSno@outlook.de>
2023-06-28 08:20:12 +0200

    add tooltip to skillmap

--- resource/app.js
+++ resource/app.js
@@ -4,20 +4,26 @@ let skillmap = document.getElementById('skillmap');
 let icons = skillmap.querySelectorAll('.icon');
 
 icons.forEach(icon => {
-    icon.addEventListener('mouseenter', highlightIcon);
+    icon.addEventListener('mouseenter', highlightIconAndDisplayTooltip);
     icon.addEventListener('mouseleave', dehighlightIcon);
-
 })
 
-function highlightIcon() {
+function highlightIconAndDisplayTooltip() {
     this.classList.add('selected');
     let selectedIcon = this.id;
     let connectedLines = skillmap.querySelectorAll('.' + selectedIcon);
     connectedLines.forEach(line => line.classList.add('selected'));
+
+    let selectedIconPositionX = this.getBoundingClientRect().left + this.getBoundingClientRect().width / 2;
+    let selectedIconPositionY = this.getBoundingClientRect().top + this.getBoundingClientRect().height / 2;
+    let selectedTooltip = skillmap.querySelector('.tooltip.' + selectedIcon);
+    selectedTooltip.style.top = selectedIconPositionY;
+    selectedTooltip.style.left = selectedIconPositionX;
 }
 
 function dehighlightIcon() {
     this.classList.remove('selected');
     let deselectedIcon = this.id;
     let connectedLines = skillmap.querySelectorAll('.' + deselectedIcon);
-    connectedLines.forEach(line => line.classList.remove('selected'));}
+    connectedLines.forEach(line => line.classList.remove('selected'));
+}
